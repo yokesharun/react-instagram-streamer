c0baebedb5a3673736ec3a05f4b0a337dc240a2c
Arun Yokesh <ayokesh.cse@gmail.com>
2021-11-18 22:56:20 +0530

    folder structure changes for package

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import InstagramStreamer from './package/src/InstagramStreamer';
-import './index.css';
+import InstagramStreamer from  './InstagramStreamer.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1 className="header">React Instagram Streamer</h1>
-    <div className="container">
-        <InstagramStreamer accessToken="IGQVJXcDMybmg5eTBiS2dpdjFyYkRMRENlREg0UzVTdjNaU01OSW9BX1Rzby10a3BST3RTbExBemtGcEhjWEVnS0xDQUQwa3NhdF9hbkdncXdVMXBZAMVFSWkRXUVNkYy1HdG9zMFE3VWVVNlJNMFlhZAwZDZD" />
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const returnLibrary = () => {
+    return {
+        InstagramStreamer: InstagramStreamer
+    }
+}
+export default returnLibrary()
--- src/package/src/index.js
+++
@@ -1,8 +0,0 @@
-import InstagramStreamer from  './InstagramStreamer.js';
-
-const returnLibrary = () => {
-    return {
-        InstagramStreamer: InstagramStreamer
-    }
-}
-export default returnLibrary()
